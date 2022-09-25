ebe0b6ab3e1fb8c5ccdc1e1d218daa5c3539a848
ketanbhoir <619ketanbhoir@gmail.com>
2022-09-25 16:09:30 +0530

    2. getting data on server

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const path = require('path');
+const bodyparser = require('body-parser');
+const User = require('./model/user');
+const { default: mongoose } = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/login-jwt-app-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const app = express();
 app.use('/', express.static(path.join(__dirname, 'static')));
+app.use(bodyparser.json());
+
+app.post('/api/register', async (req, res) => {
+  console.log(req.body);
+  res.json({ status: 'ok' });
+});
 
 app.listen(9999, () => {
   console.log('server up at 9999');
